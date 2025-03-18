395c0699c435b051d8e9482525aa888aec1a577d
andff <andffbr@gmail.com>
2025-03-18 14:24:02 -0300

    update

---
+++ Traduzir_AppSheet.py
@@ -0,0 +1,611 @@
+# IMPORTANDO BIBLIOTECAS
+import pyautogui as pg
+import time as t
+import keyboard
+
+# TEMPO ENTRE OS COMANDOS PYAUTOGUI
+pg.PAUSE = 1
+
+
+t.sleep(4)
+
+pg.press('tab')
+t.sleep(0.5)
+
+pg.press('tab')
+t.sleep(0.5)
+
+pg.write("Sim")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Não")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Novo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Editar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Deletar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Compartilhar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Sincronizar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Próximo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Anterior")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Voltar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Visualizar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Salvar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Enviar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Limpar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Cancelar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Descartar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Pronto")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Limpar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Tudo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Selecione tudo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Procurar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Adicionar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Adicionar ou procurar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Confirmar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Ok")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Você tem certeza?")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Já existe uma linha com esse registro:")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Tem certeza de que deseja fechar o aplicativo?")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Dia")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Semana")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Mês")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Hoje")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Iniciando")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Sincronizando...")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Sobre")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Mais informações")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Sair")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Tem certeza que deseja sair?")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Feedback para o criador do aplicativo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Por favor, forneça seu feedback aqui")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Opinião")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Esta entrada é obrigatória")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Esta entrada é invalida")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Selecione a fonte da foto")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Tire uma foto")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Foto")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Desenho")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Escolha entre os existentes")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Pronto para digitalizar NFC")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Toque na etiqueta NFC com a parte traseira do seu dispositivo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Toque para desbloquear")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("O resultado da verificação não correspondeu a nenhuma entrada conhecida.")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Enviar log de alterações")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Alterações pendentes")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Mostrar alterações")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Redefinir alterações")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Salvando alterações")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Não foi possivel salvar as alterações")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Descartar mudanças?")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Todas as edições serão perdidas")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Redefinir alterações locais?")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Isso descartara permanentemente todas as alterações locais.")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("As alterações locais não foram sincronizadas. Continuar mesmo assim?")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Cabeçalho de consentimento do usuário")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Descrição do consentimento das informações do dispositivo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Descrição do consentimento de uso do aplicativo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Descrição do consentimento de localizacao")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Descrição do consentimento de identidade")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Rodapé de consentimento do usuario")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Termos de uso")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Politíca de Privacidade")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Aceitar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Ir para detalhes")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Mínimo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Máximo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Da localização atual")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Contém")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("(vazio)")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Filtro")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Data de início")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Data final")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Hora de início")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Fim do tempo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Nenhum resultado")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Não há valores disponíveis")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Tem anexo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Nenhum anexo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Todos os filtros")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Nenhum item")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Nenhum item selecionado")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Crie um novo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Selecionado")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Selecione")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Desmarcar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Limpar tudo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Continuar editando")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Última sincronização:")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Alterações não sincronizadas")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Offline")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Atualizado")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Sincronização concluída")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Não foi possível sincronizar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Ver detalhes do erro")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Detalhes do erro")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Ver o status do aplicativo")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Descartar alterações não sincronizadas")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Descartar alterações")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Falha na sincronização")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Adicione uma nova opção digitando")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Cancelar seleção")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Fechar")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Habilitar edições")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Expandir")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Abrir")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Redefinir painel")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+keyboard.write("Retornar a visualização dividida")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Selecionar itens")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Mais")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
+
+pg.write("Marcar local")
+t.sleep(0.5)
+pg.press(['tab', 'tab', 'tab'])
+t.sleep(0.5)
